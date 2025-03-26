078e60b5180f497a4483d37e07bd967167671f45
Amka <amarjargal.optimal@gmail.com>
2025-03-26 23:43:41 +0800

    scope nemew

--- gym_app.py
+++ gym_app.py
@@ -9,12 +9,12 @@ import datetime
 #@st.cache_resource ашиглан Google sheets холболтыг кешлэх
 @st.cache_resource
 def connect_to_sheet():
-    #scope = ["https://www.googleapis.com/auth/spreadsheets", 
-           # "https://www.googleapis.com/auth/drive"]
+    scope = ["https://www.googleapis.com/auth/spreadsheets", 
+            "https://www.googleapis.com/auth/drive"]
     
     #creds = Credentials.from_service_account_file("D:\gym_app\json\credentials.json", scopes = scope)
     creds_dict = st.secrets["google_service_account"]
-    creds = Credentials.from_service_account_info(creds_dict)
+    creds = Credentials.from_service_account_info(creds_dict, scopes = scope)
     
     client = gspread.authorize(creds)
     sheet = client.open_by_key('1LXqGEarZMI74xgBntM_pRHUEK8cWAi7KzFL2S81Cwwo').sheet1
