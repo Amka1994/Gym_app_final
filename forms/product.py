134eb8d59cf62c50d5b9729edef94e463f67a78b
Amka <amarjargal.optimal@gmail.com>
2025-04-03 23:08:50 +0800

    яя

--- forms/product.py
+++ forms/product.py
@@ -100,4 +100,4 @@ def product_form():
                 status, payment_method, describtion, worker
             ])
             st.success("✅ Амжилттай хадгалагдлаа!")
-            st.experimental_rerun()
+            st.rerun()
